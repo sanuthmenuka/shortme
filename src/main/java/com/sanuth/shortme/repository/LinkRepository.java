a956f86cbf484cf903b36166e4d06a309bf2aa60
Menuka L.D.S <104838986+sanuthmenuka@users.noreply.github.com>
2026-01-12 09:45:00 +0530

    Add LinkRepository for database operations
    - Create JpaRepository for ShortLink entity
    - Add findByLongUrl method for duplicate URL checking

---
+++ src/main/java/com/sanuth/shortme/repository/LinkRepository.java
@@ -0,0 +1,13 @@
+package com.sanuth.shortme.repository;
+
+
+import com.sanuth.shortme.model.db.ShortLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LinkRepository extends JpaRepository<ShortLink, Long> {
+    Optional<ShortLink> findByLongUrl(String url);
+}
